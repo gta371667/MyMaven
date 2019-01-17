1c6a950d51b314fde09b12d7340b1a6ac5cf8e02
gta371667 <gta371667@gmail.com>
2019-01-17 13:26:12 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/victer/paypaypig/mylibrary/MyViewAAA.java
@@ -0,0 +1,19 @@
+package com.victer.paypaypig.mylibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class MyViewAAA extends android.support.v7.widget.AppCompatTextView {
+    public MyViewAAA(Context context) {
+        super(context);
+    }
+
+    public MyViewAAA(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyViewAAA(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
